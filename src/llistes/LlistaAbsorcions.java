98e10178fea1fecab17ff076badd6ac9d231e8db
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2022-01-10 17:50:55 +0100

    archivos prueba subido

---
+++ src/llistes/LlistaAbsorcions.java
@@ -0,0 +1,13 @@
+package llistes;
+
+public class LlistaAbsorcions {
+
+    int[] llista;
+    int nElem;
+
+    public LlistaAbsorcions(int mida) {
+        nElem = 0;
+        llista = new int[mida];
+    }
+
+}
